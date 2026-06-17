6ccd4f887605c0ef1a12658da6be60d5b08a3aa4
Florent Callaou <florent.callaou@atos.net>
2026-06-17 10:43:27 +0200

    feat: add user entity

---
+++ src/main/java/callaou/userregistration/model/entities/User.java
@@ -0,0 +1,48 @@
+package callaou.userregistration.model.entities;
+
+import java.time.LocalDate;
+
+import callaou.userregistration.model.enumerations.Country;
+import callaou.userregistration.model.enumerations.Gender;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(name = "uk_user_composite", columnNames = { "username", "birthdate", "country" })
+})
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String username;
+
+    @Column(nullable = false)
+    private LocalDate birthdate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 2)
+    private Country countryOfResidence;
+
+    @Column(length = 20)
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 30)
+    private Gender gender;
+}
