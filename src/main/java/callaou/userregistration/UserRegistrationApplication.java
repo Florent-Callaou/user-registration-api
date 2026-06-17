7b96e31c4a90aed6cb4c88e82c64561d628211ff
Florent Callaou <florent.callaou@atos.net>
2026-06-17 10:33:24 +0200

    chore: recreate application class to fix case

---
+++ src/main/java/callaou/userregistration/UserRegistrationApplication.java
@@ -0,0 +1,21 @@
+package callaou.userregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The main class for the User Registration application.
+ */
+@SpringBootApplication
+public class UserRegistrationApplication {
+
+    /**
+     * The main method to run the User Registration application.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(UserRegistrationApplication.class, args);
+    }
+
+}
