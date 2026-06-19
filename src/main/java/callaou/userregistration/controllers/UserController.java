d8040718b72595d8433bfd033ec0ec9bef0d7b85
Florent Callaou <florent.callaou@atos.net>
2026-06-19 09:59:47 +0200

    feat: add get users by criteria controller method

--- src/main/java/callaou/userregistration/controllers/UserController.java
+++ src/main/java/callaou/userregistration/controllers/UserController.java
@@ -1,7 +1,13 @@
 package callaou.userregistration.controllers;
 
+import java.util.Map;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,5 +50,21 @@ public class UserController {
         UserResponse response = userService.registerUser(userRequest);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    /**
+     * Get a page of users given filters
+     * 
+     * @param pageable the pageblae informations
+     * @param filters  the filters
+     * @return the page of users found
+     */
+    @GetMapping
+    public ResponseEntity<Page<UserResponse>> getUsersByCriteria(
+            @PageableDefault(sort = "username") Pageable pageable,
+            Map<String, Object> filters) {
+        Page<UserResponse> pageResponse = userService.findUsersByCriteria(pageable, filters);
+
+        return ResponseEntity.ok().body(pageResponse);
     }
 }
