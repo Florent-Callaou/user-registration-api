ec9053907424b72be623ada047d1006e2d738177
Florent Callaou <florent.callaou@atos.net>
2026-06-19 16:12:03 +0200

    feat: add object not found exception

--- src/main/java/callaou/userregistration/exceptions/enumerations/ErrorCode.java
+++ src/main/java/callaou/userregistration/exceptions/enumerations/ErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     BAD_REQUEST("400", HttpStatus.BAD_REQUEST),
     ALREADY_EXISTS("409", HttpStatus.CONFLICT),
+    OBJECT_NOT_FOUND("404", HttpStatus.NOT_FOUND),
     USER_NOT_ELIGIBLE("422", HttpStatus.valueOf(422)),
     INTERNAL_SERVER_ERROR("500", HttpStatus.INTERNAL_SERVER_ERROR);
 
