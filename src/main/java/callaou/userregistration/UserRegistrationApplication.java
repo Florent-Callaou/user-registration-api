e723a6eb2f421d6c6ff229ff41a50409dcfc200a
Florent Callaou <florent.callaou@atos.net>
2026-06-19 15:21:33 +0200

    fix: fix page serialization warn

--- src/main/java/callaou/userregistration/UserRegistrationApplication.java
+++ src/main/java/callaou/userregistration/UserRegistrationApplication.java
@@ -2,11 +2,13 @@ package callaou.userregistration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 /**
  * The main class for the User Registration application.
  */
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class UserRegistrationApplication {
 
     /**
